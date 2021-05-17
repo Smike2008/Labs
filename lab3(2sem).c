8d789362e32b7c88fdeb16581112bf005f5585bd
Smike2008 <71288599+Smike2008@users.noreply.github.com>
2021-05-17 17:19:15 +0300

    Update lab3(2sem).c

--- lab3(2sem).c
+++ lab3(2sem).c
@@ -1,7 +1,3 @@
-
-
-
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -22,60 +18,68 @@ int main() {
 
 		return 0;
 	}
-	int f1,i=0;
+	int f1,i=0, j ;
 	int check;
 
 	printf("Количество чисел ");
-		check=scanf("%d", &f1);
+	check = scanf("%d", &f1);
 	while (check == 0) {
 		getchar();
 		check = scanf_s("%d", &f1);
 
 	}
-		int* test;
-		test = (int*)malloc(sizeof(int));
-		system("cls");
-		
-		
+	int* test;
+	test = (int*)malloc(sizeof(int)*f1);
+	system("cls");
+
+
 	printf("Введите %d чисел\n", f1);
-		rewind(stdin);
-		for (int j = 0; j < f1; j++) {
+	rewind(stdin);
+	for (int j = 0; j < f1; j++) {
+		check = scanf("%d", &test[j]);
+		while (check == 0) {
+			getchar();
 			check = scanf("%d", &test[j]);
-			while (check == 0) {
-				getchar();
-				check=scanf("%d", &test[j]);
+		}
+		fprintf(f, " %d ", test[j]);
+
+	}
+
+	printf("Введенные числа");
+	for (j = 0; j < f1; j++) {
+
+		printf(" %d ", test[j]);
+
+	}
+	double sum = 0;
+	for (j = 0; j < f1; j++) {
+		if (test[j] > 0) {
+			sum += test[j];
+			i += 1;
+		}
+	}
+	for(i=0;i<f1;i++){
+		for (j = 0; j < f1-1; j++) {
+			if (test[j] > test[j + 1]) {
+				int k = test[j];
+				test[j] = test[j + 1];
+				test[j + 1] = k;
 			}
-			fprintf(f, " %d ", test[j]);
 
 		}
+	}
 
-		printf("Введенные числа");
-		for (int j = 0; j < f1; j++) {
+	printf("\n");
+	fclose(f);
 
-			printf(" %d ", test[j]);
-
-		}
-		double sum = 0;
-		for (int j = 0; j < f1; j++) {
-			if (test[j] > 0) {
-					sum+=test[j];
-					i += 1;
-			}
-		}
-
-		printf("\n");
-		fclose(f);
-		printf("Среднее арифметическое чисел  %.2f\n", sum/i);
+	printf("Среднее арифметическое чисел  %.2f\n", sum / i);
 
 
+	printf("Отсортированные числа:\n");
+	for (j = 0; j < f1; j++) {
+		printf(" %d ", test[j]);
+	}
 
-		system("pause");
 
-
-
-
-	
 	return 0;
 }
-
-
